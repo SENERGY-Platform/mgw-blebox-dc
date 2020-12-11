bc680c821ae82caccb0b602ca42097eb2968ab23
y-du <42994541+y-du@users.noreply.github.com>
2020-12-11 10:53:18 +0100

    remove unused conf options

--- util/config.py
+++ util/config.py
@@ -48,11 +48,8 @@ class Conf:
     @simple_env_var.section
     class Discovery:
         remote_host = None
-        device_query_delay = 10
         device_id_prefix = None
         delay = 30
-        check_delay = 60
-        check_fail_safe = 2
         timeout = 5
 
     @simple_env_var.section
